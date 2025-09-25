66d885658cfc7fb40fa3523bdb456583bf0ab39d
Victor Souza <128669807+vicss21-D@users.noreply.github.com>
2025-09-25 09:20:21 -0300

    Add files via upload

--- files_f/zoom_controller_2.v
+++ files_f/zoom_controller_2.v
@@ -1,93 +1,76 @@
 module resolution_controller (
 
-	input                CLK,
-	input                RESET,
-	input                zoom_level_button,  
-	input         [1:0]  ALGORITHM,          
+	input               CLK,
+	input               RESET,
+	input               zoom_level_button,
+	input        [1:0]  ALGORITHM,
 
-	output reg    [1:0]  SHIFT_FACTOR,       
-   output reg    [10:0] IMG_WIDTH_OUT,      
-   output reg    [9:0]  IMG_HEIGHT_OUT
+	output reg   [1:0]  SHIFT_FACTOR,
+   output reg   [10:0] IMG_WIDTH_OUT,
+   output reg   [9:0]  IMG_HEIGHT_OUT
 );
 
-	// -- PARAMETERS
+	localparam S_NN = 2'b00;
+	localparam S_PR = 2'b01;
+	localparam S_DC = 2'b10;
+	localparam S_BA = 2'b11;
 	
-	localparam S_NN = 2'b00; // Nearest Neighbor 
-	localparam S_PR = 2'b01; // Pixel Replication 
-	localparam S_DC = 2'b10; // Decimation 
-	localparam S_BA = 2'b11; // Block Averaging 
-	
-
-	localparam ZOOM_LEVEL_1X = 2'b00; // 1x
-	localparam ZOOM_LEVEL_2X = 2'b01; // 2x
-	localparam ZOOM_LEVEL_4X = 2'b10; // 4x
-	localparam ZOOM_LEVEL_8X = 2'b11; // 8x
+	localparam ZOOM_LEVEL_1X = 2'b00;
+	localparam ZOOM_LEVEL_2X = 2'b01;
+	localparam ZOOM_LEVEL_4X = 2'b10;
+	localparam ZOOM_LEVEL_8X = 2'b11;
 	
 	localparam IMG_WIDTH_IN  = 160;
    localparam IMG_HEIGHT_IN = 120;
 	
 	reg [1:0] zoom_level; 
 	
-	// Lógica para detecção de borda do botão de zoom
-	
 	reg zoom_level_button_d;
 	wire zoom_level_button_posedge = zoom_level_button && !zoom_level_button_d;
 
-	//
-
 	always @(posedge CLK or posedge RESET) begin
-	
 		if (RESET) begin
 		
-				zoom_level          <= ZOOM_LEVEL_1X; 
-            zoom_level_button_d <= 1'b0;
-				
+			zoom_level          <= ZOOM_LEVEL_1X;
+         zoom_level_button_d <= 1'b0;
+			
 		end else begin
-            
+		
             zoom_level_button_d <= zoom_level_button;
             
-            
-            if ((ALGORITHM == S_DC || ALGORITHM == S_BA) && (zoom_level > ZOOM_LEVEL_2X)) begin
+            if (ALGORITHM == S_DC || ALGORITHM == S_BA) begin
+				
+                zoom_level <= ZOOM_LEVEL_2X;
+					 
+            end else if (ALGORITHM == S_NN || ALGORITHM == S_PR) begin
+				
+                if (zoom_level_button_posedge) begin
+					 
+					case(zoom_level)
+					    ZOOM_LEVEL_1X: zoom_level <= ZOOM_LEVEL_2X;
+					    ZOOM_LEVEL_2X: zoom_level <= ZOOM_LEVEL_4X;
+					    ZOOM_LEVEL_4X: zoom_level <= ZOOM_LEVEL_8X;
+					    ZOOM_LEVEL_8X: zoom_level <= ZOOM_LEVEL_2X;
+					    default:       zoom_level <= ZOOM_LEVEL_2X;
+					endcase
+                end
+            end else begin
+				
                 zoom_level <= ZOOM_LEVEL_1X;
-            
-            
-            end else if (zoom_level_button_posedge) begin
-				if (ALGORITHM == S_NN || ALGORITHM == S_PR) begin
-                    
-					if(zoom_level == ZOOM_LEVEL_8X)
-						zoom_level <= ZOOM_LEVEL_1X;
-					else
-						zoom_level <= zoom_level + 1;
-
-				end else if (ALGORITHM == S_DC || ALGORITHM == S_BA) begin
-                    
-                    if (zoom_level == ZOOM_LEVEL_1X)
-                        zoom_level <= ZOOM_LEVEL_2X;
-                    else
-                        zoom_level <= ZOOM_LEVEL_1X;
-				end
-			end
+            end
 		end
 	end
 
-	// -- CL
-	
 	always @(*) begin
-	
         SHIFT_FACTOR = zoom_level;
 
 		if ((ALGORITHM == S_NN || ALGORITHM == S_PR) && (zoom_level > ZOOM_LEVEL_1X)) begin
-		
 			IMG_WIDTH_OUT  = IMG_WIDTH_IN << SHIFT_FACTOR;
 			IMG_HEIGHT_OUT = IMG_HEIGHT_IN << SHIFT_FACTOR;
-
 		end else if ((ALGORITHM == S_DC || ALGORITHM == S_BA) && (zoom_level > ZOOM_LEVEL_1X)) begin
-		
 			IMG_WIDTH_OUT  = IMG_WIDTH_IN >> SHIFT_FACTOR;
 			IMG_HEIGHT_OUT = IMG_HEIGHT_IN >> SHIFT_FACTOR;
-            
 		end else begin
-		
 			IMG_WIDTH_OUT  = IMG_WIDTH_IN;
 			IMG_HEIGHT_OUT = IMG_HEIGHT_IN;
 		end
